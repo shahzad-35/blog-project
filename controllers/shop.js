667c475d9ae57290a1abec5818db201f0ed0af27
shahzad-35 <shahzadhere17@gmail.com>
2022-04-27 14:27:59 +0500

    Get orders from mongoDB collection and show it on page

--- controllers/shop.js
+++ controllers/shop.js
@@ -110,7 +110,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({ include: ['products'] })
+  req.user.getOrders()
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
--- models/user.js
+++ models/user.js
@@ -82,17 +82,36 @@ class User {
             { $set: { cart: {items: updatedCartItems} } });
     }
 
-    addOrder(){
+    addOrder() {
+        const db = getDb();
+        return this.getCart()
+          .then(products => {
+            const order = {
+              items: products,
+              user: {
+                _id: new ObjectId(this._id),
+                name: this.username
+              }
+            };
+            return db.collection('orders').insertOne(order);
+          })
+          .then(result => {
+            this.cart = { items: [] };
+            return db
+              .collection('users')
+              .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: { items: [] } } }
+              );
+          });
+      }
+
+
+    getOrders(){
         const db = getDb();
         return db.collection('orders')
-        .insertOne(this.cart)
-        .then(result => {
-            this.cart = {items: []};
-            return db.collection('users')
-            .updateOne(
-            { _id: new ObjectId(this._id) },
-            { $set: { cart: {items: []} } });
-        })
+        .find({'user._id': new ObjectId(this._id)})
+        .toArray();
     }
 }
 
