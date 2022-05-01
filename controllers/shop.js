12372902382965ce001fa068fd6698179fc0d7bb
Shahzad <shahzad.ali@ilsainteractive.com>
2022-05-02 02:58:24 +0500

    getting and deleting items from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,8 +45,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -54,7 +55,7 @@ exports.getCart = (req, res, next) => {
       });
     })
     .catch(err => console.log(err));
-};
+}
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
@@ -66,42 +67,11 @@ exports.postCart = (req, res, next) => {
     console.log(result);
     res.redirect('/cart');
   })
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user.
-  //   getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } })
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = parseInt(oldQuantity) + parseInt(1);
-  //       return product;
-  //     }
-  //     return Product.findOne({ where: { 'id': prodId } })
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)    
+  req.user.removeFromCart(prodId)    
     .then(result => {
       res.redirect('/cart');
     })
--- models/user.js
+++ models/user.js
@@ -3,25 +3,52 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  cart: {
-    items:[
-        {
-            productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true},
-            quantity: {type: Number, required: true}
-        }
-    ]
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+                quantity: { type: Number, required: true }
+            }
+        ]
+    }
 });
 
-module.exports = mongoose.model('User',UserSchema);
+UserSchema.methods.addToCart = function (product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+        return JSON.stringify(cp.productId) === JSON.stringify(product._id);
+    });
+    let newQuantity = 1;
+
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+        updatedCartItems.push({ productId: product._id, quantity: newQuantity });
+    }
+
+    const updateCart = { items: updatedCartItems };
+    this.cart = updateCart;
+    return this.save();
+}
+UserSchema.methods.removeFromCart = function (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return JSON.stringify(item.productId) !== JSON.stringify(productId);
+    });
+    console.log(updatedCartItems);
+    this.cart.items = updatedCartItems;
+    return this.save();
+}
+
+module.exports = mongoose.model('User', UserSchema);
 // const mongoDb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
