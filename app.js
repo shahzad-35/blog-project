67285428af48b4582a2eb9e29b49fad81379ca61
Shahzad <shahzad.ali@ilsainteractive.com>
2022-05-06 23:22:30 +0500

    Added CSRF protection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const csrf = require('csurf');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
@@ -15,6 +16,8 @@ const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
   collection: 'sessions'
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,6 +37,8 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -44,6 +49,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+});
+
+app.use((req,res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn,
+  res.locals.csrfToken = req.csrfToken(),
+  next()
 });
 
 app.use('/admin', adminRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,9 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/login', isAuth, authController.getLogin);
+router.get('/login', authController.getLogin);
 
-router.post('/login', isAuth, authController.postLogin);
+router.post('/login', authController.postLogin);
 
 router.post('/logout', isAuth, authController.postLogout);
 
